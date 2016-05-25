5c1d8ac30e0aebf998a5c2b94a87aea943285332
samnags <sam.nagourney@gmail.com>
2016-05-25 14:35:59 -0400

    first commit

---
+++ app/models/chatroom.rb
@@ -0,0 +1,7 @@
+class Chatroom < ActiveRecord::Base
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :messages
+
+  validates :topic, presence: true, uniqueness: true, case_sensitive: false
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :messages
+  has_many :chatrooms, through :messages
+
+  validates :username, presence: true, uniqueness: true
+
+end
